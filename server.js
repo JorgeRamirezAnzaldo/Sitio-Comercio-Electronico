021d7394f74ffcc0847d151db0f8ffe876b4859d
Jorge Ramirez Anzaldo <jaanzaldo@hotmail.com>
2022-12-06 09:47:42 -0600

    Update server.js with the sequelize connection import and sync sequelize models to the DB

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
+// Import express
 const express = require('express');
+// Import the different routes
 const routes = require('./routes');
-// import sequelize connection
+// Import sequelize connection
+const sequelize = require("./config/connection");
 
+//Initialize app variable
 const app = express();
+
+//Define PORT
 const PORT = process.env.PORT || 3001;
+
+//Make the app use the routes
+app.use(routes);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
+// Sync sequelize models to the database, then turn on the server
+sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
+})
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
